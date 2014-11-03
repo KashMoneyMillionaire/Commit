964a3cec0c3b7c3a7f3d92b6ed0465ac764e92b1
kashleec <kashleec@gmail.com>
2014-11-02 22:16:51 -0600

    Staar Test for some reason

---
+++ Infrastructure/Domain/StaarTest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Domain
+{
+    public class StaarTest : EntityBase<long>
+    {
+        public int Year { get; set; }
+        public string Grade { get; set; }
+        public decimal Value { get; set; }
+        public virtual Campus Campus { get; set; }
+        public virtual Language Language { get; set; }
+        public virtual Subject Subject { get; set; }
+        public virtual CategoryDetail CategoryDetail { get; set; }
+        public virtual DemographicDetail DemographicDetail { get; set; }
+
+        public long Campus_Id { get; set; }
+        public long Language_Id { get; set; }
+        public long Subject_Id { get; set; }
+        public long CategoryDetail_Id { get; set; }
+        public long DemographicDetail_Id { get; set; }
+    }
+}
